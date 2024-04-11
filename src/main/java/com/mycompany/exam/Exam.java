24c7911acc038f422abc3bea299015a1059e8ac5
YokYoky <cvanjoseph@gmail.com>
2024-04-11 14:21:18 +0800

    JAVA GUI

---
+++ src/main/java/com/mycompany/exam/Exam.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exam;
+
+/**
+ *
+ * @author vanvan
+ */
+public class Exam {
+
+    public static void main(String[] args) {
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+}
